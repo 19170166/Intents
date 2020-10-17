b89fb94cd5a2082782b4a5ffa54eefd791314d43
angel zapata <angelzapata582@gmail.com>
2020-10-17 16:45:11 -0500

    borre el boton y solucione el cierre repentino de la app

--- app/src/main/java/com/example/cambiaractivity/inicio.java
+++ app/src/main/java/com/example/cambiaractivity/inicio.java
@@ -6,32 +6,33 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class inicio extends AppCompatActivity implements View.OnClickListener{
+import org.w3c.dom.Text;
+
+public class inicio extends AppCompatActivity{
     TextView pantalla;
-    Intent i=new Intent(getApplicationContext(),MainActivity.class);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
-        findViewById(R.id.btn1).setOnClickListener(this);
-        pantalla=findViewById(R.id.cuenta);
-    }
-
-    @Override
-    public void onClick(View v) {
-        CountDownTimer cdt=new CountDownTimer(5000,1000) {
+        pantalla=(TextView) findViewById(R.id.cuenta);
+        CountDownTimer cdt = new CountDownTimer(5000,10000) {
             @Override
             public void onTick(long segundos) {
                 pantalla.setText(String.valueOf(segundos/1000));
             }
+
             @Override
             public void onFinish() {
-            startActivity(i);
-            finish();
+                Intent i=new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(i);
+                finish();
             }
         };
         cdt.start();
     }
+
 }
